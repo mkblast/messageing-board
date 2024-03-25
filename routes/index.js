f26a4529a7478c1abd25c4f04a83d78fe9de5ddf
Karim Mk <mk47blast@protonmail.com>
2024-03-25 15:14:36 +0100

    Maybe finishing the routing and stuff.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Messages', messages: messages });
 });
+
+router.get("/new", (req, res) => {
+  res.render("form")
+})
+
+router.post("/new", (req, res) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.username,
+    added: new Date(),
+  });
+
+  res.redirect("/");
+})
 
 module.exports = router;
